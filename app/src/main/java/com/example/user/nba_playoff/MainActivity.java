6eb54ad91eb73b9ca8f24744c8683b94fcd58d78
fcu-d0240426 <honzu0927@gmail.com>
2017-04-25 21:14:33 +0800

    Push homework5 to github

---
+++ app/src/main/java/com/example/user/nba_playoff/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.user.nba_playoff;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        String sdf = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        TextView time = (TextView)findViewById(R.id.tvTime);
+        time.setText(sdf);
+
+        ArrayList<NBAitem> nbaItems = new ArrayList<NBAitem>();
+        nbaItems.add(new NBAitem(R.drawable.okc, "OKC", 99, R.drawable.hou, "HOU", 100));
+        nbaItems.add(new NBAitem(R.drawable.mem, "MEM", 80, R.drawable.sas, "SAS", 69));
+
+
+        NBAArrayAdapter adapter =
+                new NBAArrayAdapter(this, nbaItems);
+        ListView lv = (ListView)findViewById(R.id.lv);
+        lv.setAdapter(adapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
